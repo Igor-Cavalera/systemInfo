7663650112b85925a86d2ffb7f3f241d6634e802
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-05-01 05:04:12 -0300

    estilizando-rabisco

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 const os = require("os");
 const path = require("path");
-
+require("electron-reload")(__dirname);
 app.whenReady().then(main);
 
 let window;
@@ -9,8 +9,8 @@ let window;
 async function main () {
     window = new BrowserWindow({
         icon: path.join(__dirname, "/icon.ico"),
-        width: 800, 
-        height: 600,
+        width: 700, 
+        height: 580,
         autoHideMenuBar: true,
         webPreferences: {
             //devTools: false,
@@ -18,7 +18,7 @@ async function main () {
         show: false
     })   
 
-    window.webContents.openDevTools();
+    //window.webContents.openDevTools();
     
     window.on("ready-to-show", window.show);
     window.loadFile(path.join(__dirname, "/index.html"));
